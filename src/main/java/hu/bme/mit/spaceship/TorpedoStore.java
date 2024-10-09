00a7df8adcfd8896c2648058079c5eb91bba7995
tturcsanyi <tamas.f.turcsanyi@gmail.com>
2024-10-09 16:51:31 +0200

    Added a new data member: generator (Random) so no new Random at every function call

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,8 +14,12 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
+  Random generator;
+
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
+
+    generator = new Random();
 
     // update failure rate if it was specified in an environment variable
     String failureEnv = System.getenv("IVT_RATE");
@@ -36,7 +40,6 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
