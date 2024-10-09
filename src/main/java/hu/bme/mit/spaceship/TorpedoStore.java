e4241cb74550ae87226cb9c9d3b70204dafe2ee6
tturcsanyi <tamas.f.turcsanyi@gmail.com>
2024-10-09 17:19:14 +0200

    puts new Random() call in generator declaration

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,12 +14,10 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
-  Random generator;
+  Random generator = new Random();
 
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
-
-    generator = new Random();
 
     // update failure rate if it was specified in an environment variable
     String failureEnv = System.getenv("IVT_RATE");
